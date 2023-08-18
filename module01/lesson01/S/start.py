d671e9677db8ca2cf99247434dcd17a1479c6463
lexxai <lexxai@users.noreply.github.com>
2023-08-18 23:07:32 +0300

    Update start.py
    +380

--- module01/lesson01/S/start.py
+++ module01/lesson01/S/start.py
@@ -5,12 +5,12 @@ class Person:
         self.operator_code = operator_code
 
     def get_phone_number(self):
-        return f"{self.name}: +38({self.operator_code}){self.phone}"
+        return f"{self.name}: +380({self.operator_code}){self.phone}"
 
 
 if __name__ == '__main__':
 
-    person = Person("Alexander", "9995544", "050")
+    person = Person("Alexander", "9995544", "50")
     print(person.get_phone_number())
 
 # Согласно принципу Single responsibility выделить класс для телефона, адреса м т.д.
